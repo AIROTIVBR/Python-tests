b295f653669f2d3c72980b0492e79f1e294f87d0
Maria Vitória Amorim Farias Barbosa <101279006+AIROTIVBR@users.noreply.github.com>
2023-04-04 00:29:26 -0300

    Add files via upload

---
+++ aula_pratica1.py
@@ -0,0 +1,15 @@
+print("Alunos - PLP Unifavip 2023.1")
+
+num = int(input("Digite um número: "))
+print("O número informado foi ", num)
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+print(num1 + num2)
+
+nota1 = int(input("insira sua primeira nota: "))
+nota2 = int(input("insira sua segunda nota: "))
+nota3 = int(input("insira sua terceira nota: "))
+nota4 = int(input("insira sua quarta nota: "))
+media = ((nota1 + nota2 + nota3 + nota4)/4)
+print("A média é : ", media)
